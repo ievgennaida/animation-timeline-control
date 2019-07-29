ea4b36550545054ea17366c2a02178e37ae59d93
Ievgen Naida <evgenynaida@gmail.com>
2019-07-30 00:09:45 +0200

    Added typescript definition file.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# js-animation-timeline-control (1.0.1)
+# js-animation-timeline-control (1.0.2)
 
 Animation timeline is a vanilla JavaScript, no-dependency canvas control to render the animation keyframes.
 
@@ -41,6 +41,81 @@ Despite of the main options each keyframe or lane has own properties that can ov
 | ms                | keyframe position                 |
 
 Control is keeping the object references and passed payload is never modified, so you can pass additional metadata to have the related data when events are rised.
+
+### Initialization
+```
+	let defaultOptions = {
+		keysPerSecond: 60,
+		snapsPerSeconds: 5, // from 1 to 60
+		snapEnabled: true,
+		extraRightMargin: 50,
+		// Snap all the keyframes when multiple is moved.
+		snapAllKeyframesOnMove: false,
+		timelineThicknessPx: 2,
+		timelineMarginTopPx: 15,
+		timelineCapWidthPx: 4,
+		timelineCapHeightPx: 10,
+		timelineTriangleCap: false,
+		timelineRectCap: true,
+		// approximate step in px for 1 second 
+		stepPx: 120,
+		stepSmallPx: 30,
+		smallSteps: 50,
+		// additional left margin to start the gauge from
+		leftMarginPx: 25,
+		minTimelineToDispayMs: 5000,
+		headerBackground: '#101011',
+		selectedLaneColor: '#333333',
+		backgroundColor: '#101011',
+		timeIndicatorColor: 'DarkOrange',
+		labelsColor: '#D5D5D5',
+		laneLabelsColor: '#D5D5D5',
+		tickColor: '#D5D5D5',
+		selectionColor: 'White',
+		// Lanes colors
+		laneColor: '#252526', 
+		alternateLaneColor: 'black',
+		keyframesLaneColor: '#094771',
+		// keyframe color. can be overrided by a keyframe 'color' property.
+		keyframeColor: 'red',
+		// Shape of the keyframe: none|rhomb|circle|rect
+		keyframeShape: 'rhomb',
+		// selected keyframe color. can be overrider by a keyframe 'selectedColor' property.
+		selectedKeyframeColor: 'DarkOrange',
+		keyframeBorderColor: 'Black',
+		useAlternateLaneColor: false,
+		keyframeBorderThicknessPx: 0.2,
+		// can be a number or 'auto'. can be overriden by 'keyframe.size'. Auto is calculated based on the laneHeightPx.
+		keyframeSizePx: 'auto',
+		laneHeightPx: 24,
+		laneMarginPx: 2,
+		// Size of the lane in pixels. Can be 'auto' than size is based on the 'laneHeightPx'. can be overriden by lane 'lane.keyframesLaneSizePx'. 
+		keyframesLaneSizePx: 'auto',
+		headerHeight: 30,
+		lineHeight: 1,
+		autoWidth: true,
+		ticksFont: "11px sans-serif",
+		zoom: 1000,
+		// Zoom speed. Use percent of the screen to set zoom speed. 
+		zoomSpeed: 0.1,
+		// Max zoom
+		zoomMin: 80,
+		// Min zoom
+		zoomMax: 8000,
+		// scroll by drag speed (from 0 to 1)
+		scrollByDragSpeed: 0.12,
+		id: '',
+		// Use from and to range to limit the animation payload: 
+		useTimelineAnimationRange: false,
+		from: null,
+		to: null,
+		fireEventsDuringTheDrag: true,
+		// Whether keyframes draggable
+		keyframesDraggable: true,
+		// Whether keyframes lanes draggable
+		keyframesLanesDraggalbe: true
+	}
+```
 
 ## License
 
---
+++ index.d.ts
@@ -0,0 +1,94 @@
+export type TimelineEvent = 'selected' | 'timeChanged' | 'keyframeChanged' | 'scroll';
+
+export type Timeline = {
+    redraw(): void;
+    rescale(): void;
+    getOptions(): AnimationTimelineOptions;
+    setOptions(options: AnimationTimelineOptions);
+    on(event: TimelineEvent, callback: Function);
+    off(event: TimelineEvent, callback: Function);
+    getTime():number;
+    setTime(value: number);
+    nextFrame();
+    prevFrame();
+    setPanMode(value : boolean);
+}
+
+export type AnimationTimelineOptions = {
+    keysPerSecond: number,
+    snapsPerSeconds: number,
+    snapEnabled: boolean,
+    extraRightMargin: number,
+    // Snap all the keyframes when multiple is moved.
+    snapAllKeyframesOnMove: boolean,
+    timelineThicknessPx: number,
+    timelineMarginTopPx: number,
+    timelineCapWidthPx: number,
+    timelineCapHeightPx: number,
+    timelineTriangleCap: boolean,
+    timelineRectCap: boolean,
+    // approximate step in px for 1 second 
+    stepPx: number,
+    stepSmallPx: number,
+    smallSteps: number,
+    // additional left margin to start the gauge from
+    leftMarginPx: number,
+    minTimelineToDispayMs: number,
+    headerBackground: string,
+    selectedLaneColor: string,
+    backgroundColor: string,
+    timeIndicatorColor: string,
+    labelsColor: string,
+    laneLabelsColor: string,
+    tickColor: string,
+    selectionColor: string,
+    // Lanes colors
+    laneColor: string,
+    alternateLaneColor: string,
+    keyframesLaneColor: string,
+    // keyframe color. can be overrided by a keyframe 'color' property.
+    keyframeColor: string,
+    // Shape of the keyframe: none|rhomb|circle|rect
+    keyframeShape: string,
+    selectedKeyframeColor: string,
+    keyframeBorderColor: string,
+    useAlternateLaneColor: boolean,
+    keyframeBorderThicknessPx: number,
+    // can be a number or 'auto'. can be overriden by 'keyframe.size'. Auto is calculated based on the laneHeightPx.
+    keyframeSizePx: number | string,
+    laneHeightPx: number,
+    laneMarginPx: number,
+    // Size of the lane in pixels. Can be 'auto' than size is based on the 'laneHeightPx'. can be overriden by lane 'lane.keyframesLaneSizePx'. 
+    keyframesLaneSizePx: string | number,
+    headerHeight: number,
+    lineHeight: number,
+    autoWidth: boolean,
+    ticksFont: string,
+    zoom: number,
+    // Zoom speed. Use percent of the screen to set zoom speed. 
+    zoomSpeed: number,
+    // Max zoom
+    zoomMin: number,
+    // Min zoom
+    zoomMax: number,
+    // scroll by drag speed (from 0 to 1)
+    scrollByDragSpeed: number,
+    id: string,
+    // Use from and to range to limit the animation payload: 
+    useTimelineAnimationRange: boolean,
+    from: number | null,
+    to: number | null,
+    fireEventsDuringTheDrag: boolean,
+    // Whether keyframes draggable
+    keyframesDraggable: boolean,
+    // Whether keyframes lanes draggable
+    keyframesLanesDraggalbe: boolean
+}
+
+type AnimationTimeline = {
+    initialize(options: AnimationTimelineOptions): Timeline;
+};
+
+declare const timeline: AnimationTimeline;
+
+export default timeline;
