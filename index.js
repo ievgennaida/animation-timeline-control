0d185999cd8aa7b7aa6b3784d4fa2a015e979e0e
Michael Markowski <m.markowski@artifacts.de>
2020-05-04 17:58:38 +0200

    Allow scrubbing even if the playhead is not selected when mouse is in the header area. This makes it easier to scrub since the user does not have to aim the thin line.

--- index.js
+++ index.js
@@ -464,7 +464,7 @@ export default class AnimationTimeline {
             // Check whether we can drag timeline.
             var timeLinePos = valToPx(timeLine.val);
             let width = Math.max(((options.timelineThicknessPx || 1) * pixelRatio), options.timelineCapWidthPx * pixelRatio || 1) + helperSelector;
-            if (pos.x >= timeLinePos - width / 2 && pos.x <= timeLinePos + width / 2) {
+            if (pos.y <= options.headerHeight || (pos.x >= timeLinePos - width / 2 && pos.x <= timeLinePos + width / 2)) {
                 return {
                     obj: timeLine,
                     type: "timeline"
