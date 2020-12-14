5ebfcc0cf1e7a77e144c8859046acef01067cda7
Ievgen Naida <evgenynaida@gmail.com>
2020-12-14 22:45:19 +0100

    versions update. Webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,61 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
 
 //  devtool: 'inline-source-map',
-module.exports = {
-  mode: 'production',
-  entry: {
-    'animation-timeline': './src/animation-timeline.ts',
+module.exports = [
+  {
+    mode: 'production',
+    name: 'minimized-prod',
+    entry: {
+      'animation-timeline': './src/animation-timeline.ts',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'babel-loader',
+          exclude: [/node_modules/, /lib/],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts'],
+    },
+    output: {
+      filename: '[name].min.js',
+      libraryTarget: 'umd',
+      library: 'timelineModule',
+      // eslint-disable-next-line no-undef
+      path: path.resolve(__dirname, 'lib'),
+    },
   },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'babel-loader',
-        exclude: [/node_modules/, /lib/],
-      },
-    ],
+  {
+    mode: 'production',
+    name: 'unminimized',
+    entry: {
+      'animation-timeline': './src/animation-timeline.ts',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'babel-loader',
+          exclude: [/node_modules/, /lib/],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts'],
+    },
+    optimization: {
+      minimize: false,
+    },
+    output: {
+      filename: '[name].js',
+      libraryTarget: 'umd',
+      library: 'timelineModule',
+      // eslint-disable-next-line no-undef
+      path: path.resolve(__dirname, 'lib'),
+    },
   },
-  resolve: {
-    extensions: ['.tsx', '.ts'],
-  },
-  output: {
-    filename: '[name].min.js',
-    libraryTarget: 'umd',
-    library: 'timelineModule',
-    // eslint-disable-next-line no-undef
-    path: path.resolve(__dirname, 'lib'),
-  },
-  plugins: [new UnminifiedWebpackPlugin()],
-};
+];
