14b4d5a8a38c1c421dc9e45703c740e57a206582
Gavin <gavinjboyd@gmail.com>
2021-07-14 18:33:27 +0900

    add empty component ReactTimelineControl

---
+++ react-example/src/Components/ReactTimelineControl.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ReactTimelineControl from './ReactTimelineControl';
+
+test('renders react timeline control component', () => {
+  render(<ReactTimelineControl />);
+});
---
+++ react-example/src/Components/ReactTimelineControl.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function ReactTimelineControl() {
+  return (
+    <div>
+      Timeline Control
+    </div>
+  );
+}
+
+export default ReactTimelineControl;
