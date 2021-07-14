b157fac9d231b980dd5394ba23210b61ca220bf0
Gavin <gavinjboyd@gmail.com>
2021-07-14 21:40:01 +0900

    modify types to match what is expected from code

--- lib/timelineRow.d.ts
+++ lib/timelineRow.d.ts
@@ -3,4 +3,5 @@ import { TimelineRowStyle } from './settings/styles/timelineRowStyle';
 import { TimelineRanged } from './timelineRanged';
 export interface TimelineRow extends TimelineRowStyle, TimelineRanged {
     keyframes?: Array<TimelineKeyframe>;
+    title?: string;
 }
