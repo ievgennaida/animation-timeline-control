5ee25f5c8b6c96a55ff39f0ec4654634270bab1c
Gavin <gavinjboyd@gmail.com>
2021-07-17 12:47:57 +0900

    remove the react example folder

--- react-example/src/Components/ReactTimeline/ReactTimeline.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import ReactTimeline from './ReactTimeline';
-import {rows} from '../../testData';
-
-test('renders react timeline control component', () => {
-  render(<ReactTimeline rows={rows}  />);
-});
